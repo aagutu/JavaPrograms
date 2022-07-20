ac7275114bce9fcc3d7189868bfda86ad3e5baa5
aagutu <allanagutu@gmail.com>
2022-07-20 22:44:27 +0300

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //Prints "hello world" to the terminal window
+        System.out.println("Hello World");
+    }
+}
